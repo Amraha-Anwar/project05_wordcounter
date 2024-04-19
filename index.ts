333e1d6bc8a1081e64dd73bf0be62b13e249f6ab
ZubairAnwer <zubairmohomad4@gmail.com>
2024-04-19 17:59:25 +0500

    this is project of word counter

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+ 
+console.log(`\n\t"Hey there! WELCOME TO  MY PROJECT"\t\n`);
+
+const user_answer: {
+
+     userInput: string
+}= await inquirer.prompt(
+    [
+        {
+            name: "userInput",
+            type: "input",
+            message: "Enter your input to count the words.."
+        }
+    ]
+) 
+let words =user_answer.userInput.trim().split(" ")
+console.log(words);
+console.log(`There are ${words.length} words in your given input.`);
